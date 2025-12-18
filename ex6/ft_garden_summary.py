470a8a4b00059d81c571cb9bb6ef540e04833ea9
out-0 <ayoubarid000@gmail.com>
2025-12-18 02:35:47 +0100

    Complete the Module00

--- ex6/ft_garden_summary.py
+++ ex6/ft_garden_summary.py
@@ -1,6 +1,7 @@
 def ft_garden_summary() -> None:
     garden_name = input("Enter garden name: ")
     plants_count = input("Enter number of plants: ")
+
     print(f"Garden: {garden_name}")
     print(f"Plants: {plants_count}")
     print("Status: Growing well!")
