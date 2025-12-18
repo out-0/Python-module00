08021bfa714df6c446483c2f568a97cc21cf2c8d
out-0 <ayoubarid000@gmail.com>
2025-12-18 15:45:07 +0100

    update

--- Module01/ex1/main.py
+++ Module01/ex1/main.py
@@ -1,17 +1,23 @@
+
 class Person:
-    pass
+    species = "human"
 
 p1 = Person()
 p2 = Person()
 
-print(id(p1))
-print(id(p2))
-print(id(Person))
+print("Before:")
+print(f"p1.species: {p1.species}, id: {id(p1.species)}")
+print(f"p2.species: {p2.species}, id: {id(p2.species)}")
+print(f"Person.species: {Person.species}, id: {id(Person.species)}")
 
-print(type(Person))
-print(type(p1))
-print(type(p2))
+# Now let's "change" p1's species
+p1.species = "alien"
 
-print(dir(Person))
-print(dir(p1))
-print(dir(p2))
+print("\nAfter p1.species = 'alien':")
+print(f"p1.species: {p1.species}, id: {id(p1.species)}")
+print(f"p2.species: {p2.species}, id: {id(p2.species)}")
+print(f"Person.species: {Person.species}, id: {id(Person.species)}")
+
+print("\nLet's check what's inside each object:")
+print(f"p1.__dict__: {p1.__dict__}")
+print(f"p2.__dict__: {p2.__dict__}")
