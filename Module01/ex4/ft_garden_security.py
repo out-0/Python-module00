de26d9df62860cfb9bac1af082d461aa3f85fd49
out-0 <ayoubarid000@gmail.com>
2025-12-20 00:16:41 +0100

    Done ex4 Module01

--- Module01/ex4/ft_garden_security.py
+++ Module01/ex4/ft_garden_security.py
@@ -1,3 +1,57 @@
 class SecurePlant:
     """Security class provide methods to protecte invalid status"""
-    
+
+    def __init__(self, name: str, height: int, age: int):
+        """Constructor that initialize the object with the base status
+        and check for the invalid values."""
+        self.name = name
+        if height < 0:
+            self._height = 0
+        else:
+            self._height = height
+        if age < 0:
+            self._age = 0
+        else:
+            self._age = age
+        print(f"Plant created: {name}")
+
+    def set_height(self, height: int):
+        """Height setter that check for invalid values"""
+        if height > 0:
+            self._height = height
+            print(f"Height updated: {height}cm [OK]")
+        else:
+            print(f"Invalid operation attempted: height {height}cm [REJECTED]")
+            print("Security: Negative height rejected")
+
+    def set_age(self, age: int):
+        """Age setter that check for invalid values"""
+        if age > 0:
+            self._age = age
+            print(f"Age updated: {age} days [OK]")
+        else:
+            print(f"Invalid operation attempted: age {age} days [REJECTED]")
+            print("Security: Negative age rejected")
+
+    def get_height(self):
+        """Height getter that return the height value"""
+        return self._height
+
+    def get_age(self):
+        """Age getter that return the height value"""
+        return self._age
+
+    def get_info(self):
+        """Display current information about the plant"""
+        print(f"Current plant: {self.name}", end="")
+        print(f" ({self._height}cm, {self._age} days)")
+
+
+print("=== Garden Security System ===")
+plant = SecurePlant("Rose", 20, 30)
+plant.set_height(25)
+plant.set_age(30)
+print("")
+plant.set_height(-5)
+print("")
+plant.get_info()
